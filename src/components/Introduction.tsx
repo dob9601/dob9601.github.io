5f052f7e12821351e9bacee4e9e4db2c1e152ad7
Daniel O'Brien <dob9601@gmail.com>
2023-02-03 14:49:51 +0000

    Update intro

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,7 +4,7 @@ export function Introduction() {
     return (
         <div className="Introduction">
             <h2 className="tagline"><i>Software Developer / Student / Discord Bot Enthusiast</i></h2>
-            <p>Hey there! I'm a computer science student at the University of York and associate software engineer at Genomics PLC.</p>
+            <p>Hey there! I'm a final year computer science student at the University of York and ex-software engineer at Genomics PLC.</p>
             <p>When I'm not sat around trying to figure out how to write an about me page, I enjoy (writing code), swimming, gaming and hiking (yes, that outline is me &#128578;).</p>
         </div>
     )
